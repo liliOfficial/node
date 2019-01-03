a01968a400c08d6271a38d0051ef0c7b6264687b
Li Li <li.li@cash-admins-MacBook-Pro.local>
2019-01-03 17:44:14 +1100

    commit

--- init/socket.js
+++ init/socket.js
@@ -1,28 +1,54 @@
-
 const socketIO = require('socket.io');
 const moment = require('moment');
+const { Users } = require('../models/room');
 
 module.exports = function(server) {
-  var io = socketIO(server);
+  const io = socketIO(server);
+  const users = new Users();
   io.on('connection', socket => {
     console.log('New user connected');
+    socket.on('roomEnter', params => {
+      if (params.userName && params.roomName) {
+        socket.join(params.roomName);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.userName, params.roomName);
+
+        io.to(params.roomName).emit(
+          'updateUserList',
+          users.getUserList(params.roomName)
+        );
+        socket.broadcast.to(params.roomName).emit('newEnter', params);
+      }
+    });
 
     socket.on('createMessage', (message, callback) => {
-      var formatTime = moment().format('DD/MM/YYYY, h:mm:ss a');
-      message.time = formatTime;
-      io.emit('newMessage', message);
-      // socket.broadcast.emit('newMessage', email);
-      callback('Welcome for the server');
+      const user = users.getUser(socket.id);
+      if (user) {
+        message.from = user.name;
+        const formatTime = moment().format('DD/MM/YYYY, h:mm:ss a');
+        message.time = formatTime;
+        io.to(user.room).emit('newMessage', message);
+        return;
+      }
+
+      callback('Welcome to the server');
     });
 
     socket.on('createLocationMessage', coords => {
       io.emit('newLocation', {
-          url:`https://www.google.com.au/maps?q=${coords.latitude},${coords.longitude}`
+        url: `https://www.google.com.au/maps?q=${coords.latitude},${
+          coords.longitude
+        }`
       });
     });
 
     socket.on('disconnect', () => {
-      console.log('Disconnected from server');
+      const user = users.removeUser(socket.id);
+
+      if (user) {
+        io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+        io.to(user.room).emit('newLeave', user.name);
+      }
     });
   });
 };
--- public/main.js
+++ public/main.js
@@ -1,18 +1,42 @@
 var socket = io();
 var query = jQuery.deparam(window.location.search);
-var roomName = query['room-name'].trim();
-if (!roomName) {
+var userName = query['user-name'];
+var roomName = query['room-name'];
+
+if (!roomName.trim() || !userName.trim()) {
   window.location.href = '/';
 } else {
   $('#room').text(roomName);
 }
 
 socket.on('connect', function() {
-  var params = jQuery.deparam(window.location.search);
+  socket.emit('roomEnter', {
+    userName: userName,
+    roomName: roomName
+  });
 });
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
+
+socket.on('updateUserList', function(users) {
+  $('#users').text('');
+  users.forEach(user => {
+    var li = $('<li>' + user + '</li>');
+    $('#users').append(li);
+  });
+});
+
+socket.on('newEnter', function(params) {
+  var message = params.userName + ' enter ' + roomName;
+  systemMessage(message);
+});
+socket.on('newLeave', function(user) {
+  console.log(user);
+  var message = user + ' leave ' + roomName;
+  LeaveMessage(message);
+});
+
 socket.on('newMessage', function(content) {
   var user = $('<b>' + content.from + '</b>');
   var time = $('<small class=" ml-2 text-muted">' + content.time + '</small>');
@@ -31,24 +55,16 @@ socket.on('newLocation', function(content) {
 
 $('#submit-btn').click(function(e) {
   e.preventDefault();
-  var name = $('#name').val();
   var message = $('#message').val();
-
-  if (!name) {
-    $('#namevali').text('Please enter your name');
-  } else {
-    $('#namevali').text('');
-  }
   if (!message) {
     $('#messagevali').text('Please enter the message');
   } else {
     $('#messagevali').text('');
   }
-  if (name && message) {
+  if (userName && message) {
     socket.emit(
       'createMessage',
       {
-        from: name,
         message: message,
         time: new Date()
       },
@@ -77,4 +93,22 @@ $('#location').click(function() {
 
 function scrollToBottom() {
   $('#output').scrollTop($('#output')[0].scrollHeight);
+}
+
+function systemMessage(message) {
+  var badage = $(
+    '<div class="badge badge-pill badge-success my-1">' +
+      message +
+      '</div><br/>'
+  );
+  $('#output').append(badage);
+  scrollToBottom();
+}
+
+function LeaveMessage(message) {
+  var badage = $(
+    '<div class="badge badge-pill badge-danger my-1">' + message + '</div><br/>'
+  );
+  $('#output').append(badage);
+  scrollToBottom();
 }
